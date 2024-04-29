17bfa615d96c93e2b908ccb54f05621ee4eab25d
jeeshan12 <mohdjeeshan007@gmail.com>
2024-04-29 18:36:54 +0530

    solved removeDuplicatesFromLinkedList

---
+++ src/linkedlist/RemoveDuplicatesFromLinkedList.java
@@ -0,0 +1,27 @@
+package linkedlist;
+
+public class RemoveDuplicatesFromLinkedList {
+  public static class LinkedList {
+    public int value;
+    public LinkedList next;
+
+    public LinkedList(int value) {
+      this.value = value;
+      this.next = null;
+    }
+  }
+
+  public LinkedList removeDuplicatesFromLinkedList(LinkedList linkedList) {
+    LinkedList temp = linkedList;
+
+    while (temp.next != null) {
+      if (temp.value == temp.next.value) {
+        temp.next = temp.next.next;
+      } else {
+        temp = temp.next;
+      }
+
+    }
+    return linkedList;
+  }
+}
